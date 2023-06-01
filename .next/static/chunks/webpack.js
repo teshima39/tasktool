227c22a3dcfeed91462ca03174341934f99e2ed8
teshima39 <ty39teshima@gmail.com>
2023-06-01 13:46:49 +0900

    useNowDate修正

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cab5980b22bd8401"; }
+/******/ 		__webpack_require__.h = function() { return "f83df308887ef5cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- hook/useNowDate.tsx
+++ hook/useNowDate.tsx
@@ -1,8 +1,7 @@
-import {format} from 'date-fns-tz'
+import { format } from 'date-fns';
 
 export const useNowDate = () => {
-  let d  =format(new Date(), 'yyyy-MM-dd HH:mm', { timeZone: 'UTC' })
+  let d = format(new Date(), 'yyyy-MM-dd HH:mm');
 
-  return (`${d}`)
+  return `${d}`;
 }
-
