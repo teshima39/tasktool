c23cf4ffce5d4deda07ff24d5b6943e5d1c5cdb7
teshima39 <ty39teshima@gmail.com>
2023-06-01 13:02:46 +0900

    useNowDate修正

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "351c6e63e77d9d08"; }
+/******/ 		__webpack_require__.h = function() { return "b05b3dd675342df8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- hook/useNowDate.tsx
+++ hook/useNowDate.tsx
@@ -1,8 +1,17 @@
-import format from 'date-fns/format'
+import {utcToZonedTime} from 'date-fns-tz'
 
 export const useNowDate = () => {
-  const now = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
-  const date = format(now, 'yy/MM/dd hh:mm')
-  return (`${date}`)
+  let d  = utcToZonedTime(new Date(), 'Asia/Tokyo');
+  let year = Number(d.getFullYear().toString().slice(-2));
+  let month = d.getMonth() + 1;
+  let day = d.getDate();
+  let dayofweek = d.getDay();
+
+  const dayname = ['日', '月', '火', '水', '木', '金', '土'];
+
+  let hour = d.getHours().toString().padStart(2, '0');
+  let minute = d.getMinutes().toString().padStart(2, '0');
+
+  return (`${year + '年' + month + '月' + day + '日' + '[' + dayname[dayofweek] + ']' + hour + ':' + minute}`)
 }
 
