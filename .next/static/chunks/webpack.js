1470dbd09f83fb99d4a77240ba243b147d00c68e
teshima39 <ty39teshima@gmail.com>
2023-06-01 12:47:37 +0900

    useNowDate修正

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "84fd3241867ea13e"; }
+/******/ 		__webpack_require__.h = function() { return "426e9566337bc517"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- hook/useNowDate.tsx
+++ hook/useNowDate.tsx
@@ -2,9 +2,7 @@ import format from 'date-fns/format'
 
 export const useNowDate = () => {
   const now = new Date()
-  const date = format(now, 'yyyy/MM/dd')
-
-
+  const date = format(now, 'yy/MM/dd hh:mm')
   return (`${date}`)
 }
 
